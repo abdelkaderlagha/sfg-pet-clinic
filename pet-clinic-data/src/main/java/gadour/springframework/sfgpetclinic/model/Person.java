9803ed77149ebef2d32d4755e144db13b82d44d1
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-12 23:13:19 +0100

    added id long to model. closes #10

--- pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package gadour.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends  BaseEntity{
 
     private String firstName;
     private String lastname;
--- pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package gadour.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends  BaseEntity{
 
     private String name;
 
