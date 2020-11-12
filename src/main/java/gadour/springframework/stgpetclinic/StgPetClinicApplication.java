59901ed175d82f1b3aa3b6a08ed28922e077303f
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-12 19:47:15 +0100

    Initial project Add

---
+++ src/main/java/gadour/springframework/stgpetclinic/StgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package gadour.springframework.stgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gadour/springframework/stgpetclinic/StgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package gadour.springframework.stgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
