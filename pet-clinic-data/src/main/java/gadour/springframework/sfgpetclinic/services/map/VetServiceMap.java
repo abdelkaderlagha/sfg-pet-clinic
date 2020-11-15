b615bb96dfc279f7a0bcfaea3e18c9f01b6da3c0
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-15 03:21:44 +0100

    closes #6

---
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package gadour.springframework.sfgpetclinic.services.map;
+
+import gadour.springframework.sfgpetclinic.model.Vet;
+import gadour.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet,Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
