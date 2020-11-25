657b9aa232c187c02f88e85000ba4e8e56a1506b
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-25 16:33:58 +0100

    closes #27

--- pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/model/Vet.java
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/model/Vet.java
@@ -1,4 +1,16 @@
 package gadour.springframework.sfgpetclinic.model.model;
 
+import java.util.Set;
+
 public class Vet extends  Person {
+
+    private Set<Speciality> speciality ;
+
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
