cec20d82dc2b6ce0b009519f12b7e6d2684f4a82
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-25 16:36:35 +0100

    closes #28

--- pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/model/Owner.java
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/model/Owner.java
@@ -4,7 +4,35 @@ import java.util.Set;
 
 public class Owner extends  Person{
 
+    private String Adress;
+    private String telephone;
+    private String city;
+
     private Set<Pet> pets;
+
+    public String getAdress() {
+        return Adress;
+    }
+
+    public void setAdress(String adress) {
+        Adress = adress;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
