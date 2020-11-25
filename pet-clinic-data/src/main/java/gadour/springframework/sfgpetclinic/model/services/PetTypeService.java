b2daa868a34e5077def3f79c6fd2f723060f4aaa
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-11-25 16:43:20 +0100

    closes #29

---
+++ pet-clinic-data/src/main/java/gadour/springframework/sfgpetclinic/model/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package gadour.springframework.sfgpetclinic.model.services;
+
+import gadour.springframework.sfgpetclinic.model.model.PetType;
+
+public interface PetTypeService extends CrudService <PetType, Long> {
+}
