96ef498f0fdf5168311a2a79a06038684f61a059
abdelkaderlagha <abdelkaderlaghaa@outlook.fr>
2020-12-02 13:44:53 +0100

    added CircleCi setup.

--- pet-clinic-web/src/test/java/gadour/springframework/sfgpetclinic/model/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/gadour/springframework/sfgpetclinic/model/controllers/OwnerControllerTest.java
@@ -1,19 +1,56 @@
 package gadour.springframework.sfgpetclinic.model.controllers;
 
+import gadour.springframework.sfgpetclinic.model.model.Owner;
+import gadour.springframework.sfgpetclinic.model.services.OwnerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.actuate.info.InfoPropertiesInfoContributor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.validateMockitoUsage;
+import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    Set<Owner> ownerSet;
+
+    MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
+        ownerSet = new HashSet<>();
+        ownerSet.add(Owner.builder().id(1L).build());
+        ownerSet.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(ownerController)
+                .build();
+
+
     }
 
     @Test
     void listOwner() {
+        when(ownerService.findAll()).thenReturn(ownerSet);
+
     }
 
     @Test
